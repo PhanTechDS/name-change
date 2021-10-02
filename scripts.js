e94c0d5bbaa86d352736e675468167cecd54b4f2
PhanTechDS <akashchatterjee2501@outlook.com>
2021-10-02 12:52:42 +0530

    All updated

--- scripts.js
+++ scripts.js
@@ -4,11 +4,6 @@ const change = document.querySelector(".heading");
 const add = document.querySelector(".done");
 
 button.addEventListener("click", function () {
-  // if (input.value == "" || input.value.match(/^ *$/) !== null) {
-  //   change.innerHTML = "Hello, Akash";
-  //   add.innerHTML = "❗ ❗ ❗ Input Name ❗ ❗ ❗";
-  // } else {
-  change.innerHTML = "Hello, " + input.value;
-  add.innerHTML = "☑️Name Changed";
-  // }
+  change.innerHTML = "Hello," + input.value;
+  add.innerHTML = "Name changed";
 });
